3961671f47ffd957855baa0785ef45ce4eab228c
Joshua Herrera <joshua.herrera@outlook.com>
2020-02-08 19:09:14 -0800

    added justinas/alice packaged for cleaner middleware creation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	// use for all route declarations
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", app.home)
@@ -13,6 +18,8 @@ func (app *application) routes() http.Handler {
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// pass servemux as param to middleware. Fcn returns http.Handler
-	// so no need to do anything
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	// so no need to do anything. Alice package essentially looks like
+	// this commented out code.
+	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
